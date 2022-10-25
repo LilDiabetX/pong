2f92cb21710e091a6532ae1e52ed012e30f4bfc0
Benjamin Decker <benjamin.t.decker@gmail.com>
2022-10-25 11:25:04 +0200

    Ajout de commentaires pour les classes Sound et EvenementAleatoire

--- src/main/java/model/EvenementAleatoire.java
+++ src/main/java/model/EvenementAleatoire.java
@@ -2,6 +2,7 @@ package model;
 
 public class EvenementAleatoire{
 	
+	// Les évènements aléatoires ont une position en abscisse (posX) et une position en ordonnée (posY). Ils ont également un Court sur lequel il seront créés
 	private double posX;
 	private double posY;
 	private Court court;
@@ -13,10 +14,12 @@ public class EvenementAleatoire{
 		this.posY = (Math.random()*(this.court.getHeight()));
 	}
 
+	// renvoie la position en abscisse de l'évènement aléatoire
 	public double getPosX(){
 		return this.posX;
 	}
 
+	// renvoie la position en ordonnée de l'évènement aléatoire
 	public double getPosY(){
 		return this.posY;
 	}

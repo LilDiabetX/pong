0882d953c2336fb2d3967dfe2e3f69013329223b
mathusan selvakumar <mathusan.selvakumar@gmail.com>
2022-10-25 13:22:53 +0200

    Revamped class PowerUp

--- src/main/java/model/Ball.java
+++ src/main/java/model/Ball.java
@@ -5,11 +5,17 @@ public class Ball{
 	private double ballX, ballY; // m 
 	private double ballSpeedX, ballSpeedY; // m
 
-	public Ball(double ballX, double ballY, double ballSpeedX, double ballSpeedY){
-		this.ballX=ballX;
-		this.ballY=ballY;
-		this.ballSpeedX=ballSpeedX;
-		this.ballSpeedY=ballSpeedY;
+    private Racket playerA, playerB;
+
+    private boolean isLastHitPlayerB;       // false at the beginning (must be changed)
+
+	public Ball(double ballX, double ballY, double ballSpeedX, double ballSpeedY, Racket playerA, Racket playerB){
+		this.ballX = ballX;
+		this.ballY = ballY;
+		this.ballSpeedX = ballSpeedX;
+		this.ballSpeedY = ballSpeedY;
+        this.playerA = playerA;
+        this.playerB = playerB;
 	}
 
 	public double getBallRadius() {
@@ -32,19 +38,39 @@ public class Ball{
         return ballSpeedY;
     }
 
+    public Racket getHitBy() {
+        if (isLastHitPlayerB) {
+            return playerB;
+        } else {
+            return playerA;
+        }
+    }
+
+    public Racket getNextReceivedBy() {
+        if (isLastHitPlayerB) {
+            return playerA;
+        } else {
+            return playerB;
+        }
+    }
+
     public void setBallX(double ballX) {
-        this.ballX=ballX;
+        this.ballX = ballX;
     }
 
     public void setBallY(double ballY) {
-        this.ballY=ballY;
+        this.ballY = ballY;
     }
 
     public void setBallSpeedX(double ballSpeedX) {
-        this.ballSpeedX=ballSpeedX;
+        this.ballSpeedX = ballSpeedX;
     }
 
     public void setBallSpeedY(double ballSpeedY) {
-        this.ballSpeedY=ballSpeedY;
+        this.ballSpeedY = ballSpeedY;
+    }
+
+    public void invertLastHitBy() {
+        isLastHitPlayerB = !isLastHitPlayerB;
     }
 }

c33a13e0ff3932e77f4176d0ac66c48ba2fb404b
GUETTEVILLE nathan <nathan.guetteville@etu.u-paris.fr>
2022-12-29 22:52:03 +0100

    revert

--- src/main/java/model/Countdown.java
+++ src/main/java/model/Countdown.java
@@ -67,9 +67,15 @@ package model;
 
 	            public void run() {
 					
+	                second--;
+					ddSecond =dFormat.format(second);
+					ddMinute= dFormat.format(minute);
+					
+				
 	                if (minute==0 && second==0) {
 	                    isEnd=true;
 						timer.cancel();
+	                   System.out.println("over");
 	                }
 
 					if(second==0&& minute!=0){
@@ -78,11 +84,6 @@ package model;
 						ddSecond =dFormat.format(second);
 						ddMinute= dFormat.format(minute);
 					}
-
-					second--;
-					ddSecond =dFormat.format(second);
-					ddMinute= dFormat.format(minute);
-
 	            }
 	        }, 0, 1000);
 				
